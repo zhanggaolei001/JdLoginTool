2c8437734fd71915581905112d519fff74252348
高磊 张 <zhanggaolei@qq.com>
2021-09-02 14:28:04 +0800

    新增自动上传到服务器的代码及配置,默认是关闭的,有示例

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
 using System.Windows;
 using CefSharp;
+using RestSharp;
 
 namespace JdLoginTool.Wpf
 {
@@ -15,17 +17,37 @@ namespace JdLoginTool.Wpf
 
         private void Browser_TitleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            string str = "";
+            string ck = "";
             this.Browser.Dispatcher.Invoke(new Action(() =>
             {
                 ICookieManager cm = Browser.WebBrowser.GetCookieManager();
                 var visitor = new TaskCookieVisitor();
                 cm.VisitAllCookies(visitor);
-                str = visitor.Task.Result.Where(cookie => cookie.Name == "pt_key" || cookie.Name == "pt_pin").Aggregate(str, (current, cookie) => current + $"{cookie.Name}={cookie.Value};");
-                if (str.Contains("pt_key") && str.Contains("pt_pin"))
+                ck = visitor.Task.Result.Where(cookie => cookie.Name == "pt_key" || cookie.Name == "pt_pin").Aggregate(ck, (current, cookie) => current + $"{cookie.Name}={cookie.Value};");
+                if (ck.Contains("pt_key") && ck.Contains("pt_pin"))
                 {
-                    Clipboard.SetText(str);
-                    MessageBox.Show(str, "Cookie已复制到剪切板");
+                    Clipboard.SetText(ck);
+                    var upload = ConfigurationManager.AppSettings["upload"] == "true";
+                    var ckServer = ConfigurationManager.AppSettings["server"]; 
+                    if (upload && !string.IsNullOrWhiteSpace(ckServer))
+                    { 
+                        var method = ConfigurationManager.AppSettings["method"];
+                        try
+                        {
+                            var client = new RestClient("ckServer" + ck)
+                            {
+                                Timeout = -1
+                            };
+                            var request = new RestRequest(method == "post" ? Method.POST : Method.GET);
+                            client.Execute(request);
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message);
+                        }
+
+                    }
+                    MessageBox.Show(ck, "Cookie已复制到剪切板");
                     Application.Current.Shutdown();
                 }
             }));
